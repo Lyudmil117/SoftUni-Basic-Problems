dedceb0967e4c6b8f8eab6957daef477f1832f44
Lyudmil Nakashev <110760786+Lyudmil117@users.noreply.github.com>
2022-08-12 10:53:42 +0300

    Add files via upload
    some new tasks from Telerik academy

---
+++ anagram.py
@@ -0,0 +1,15 @@
+
+list = ["232", "mishka", "121", "muha"]
+new_list = []
+
+for w in list:
+    new_list.append(w)
+
+print(new_list)
+for word in new_list:
+    rev_word = word[::-1]
+    if rev_word == word:
+        print("Yes")
+    else:
+        print("No")
+
