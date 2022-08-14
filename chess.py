c5513bfc1430491a9751d5fc6bf17b46cec21dea
Lyudmil Nakashev <110760786+Lyudmil117@users.noreply.github.com>
2022-08-14 11:11:24 +0300

    determine chessboard colour

---
+++ chess.py
@@ -0,0 +1,10 @@
+width = input()
+hight = int(input())
+
+
+if width == "a" or width == "c" or width == "e" or width == "g":
+    if hight == 1 or hight == 3 or hight == 5 or hight == 7:
+        print("black")
+else:
+    print("white")
+
